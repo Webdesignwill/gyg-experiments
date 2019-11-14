e3c463a42063a8feda80389d07996692587de0ae
Will Falkowski <will@webdesignwill.io>
2019-11-14 15:18:46 +0100

    addition of sidebar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
- 
+class SidebarNav {
+  constructor() {
+
+    this.bodyEl = document.querySelector('body')
+
+    this.addEvents(
+      document.querySelectorAll('[data-control="toggle-sidebar"]'),
+      'sidebar-active'
+    )
+  }
+
+  addEvents(toggle, className) {
+    for(let i = 0; i< toggle.length; i++) {
+      toggle[i].addEventListener('click', () => {
+        this.bodyEl.classList.toggle(className)
+      })
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if(document.querySelector('[data-component="sidebar"]')) {
+    new SidebarNav()
+  }
+}, false)
